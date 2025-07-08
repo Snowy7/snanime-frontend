57b418998e92b3af8d8082069126db4901e07d23
Snowy <97126807+Snowy7@users.noreply.github.com>
2025-07-08 23:30:47 +0300

    updated the og image and home metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,17 @@ import HomeClient from "@/components/pages/HomeClient";
 export const metadata: Metadata = {
   title: "Home",
   description: "SnAnime - Your ultimate destination for streaming high-quality anime. Discover new series, watch the latest episodes, and immerse yourself in the world of anime.",
+  metadataBase: new URL("https://snanime.snowydev.xyz"),
+  openGraph: {
+    title: "SnAnime",
+    description: "SnAnime - Your ultimate destination for streaming high-quality anime. Discover new series, watch the latest episodes, and immerse yourself in the world of anime.",
+    images: ["/og-image.png"],
+  },
+  twitter: {
+    title: "SnAnime",
+    description: "SnAnime - Your ultimate destination for streaming high-quality anime. Discover new series, watch the latest episodes, and immerse yourself in the world of anime.",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function Home() {
