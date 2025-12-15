5e5b908f4161821bb4e7588fefa8058ca6d2aa7d
Snowy <snowydev7@gmail.com>
2025-12-15 19:03:16 +0300

    updated

--- next.config.ts
+++ next.config.ts
@@ -3,30 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [
-      "encrypted-tbn0.gstatic.com",
-      "i.ebayimg.com",
-      "image.tmdb.org",
-      "m.media-amazon.com",
-      "cdn.europosters.eu",
-      "images-cdn.ubuy.qa",
-      "i.pinimg.com",
-      "s4.anilist.co",
-      "kaa.mx",
-      "cdn.myanimelist.net",
-      "anime4up.rest",
-      "cdn.anime-planet.com",
-      "raw.githubusercontent.com",
-      "cdn.noitatnemucod.net",
-      "img.anslayer.com",
-      "animeowl.me",
-      "app.sanime.net",
-      "witanime.pics",
-      "static.anikai.to",
-      "witanime.red",
-      "cdn.animenewsnetwork.com",
-      "*"
-    ]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   }
 };
 
